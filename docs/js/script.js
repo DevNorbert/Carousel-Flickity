abd9b3eab7543fcabad44029b88d0181e69b262f
Norbert Janczak <norbert.janczak03@gmail.com>
2019-01-15 22:15:40 +0100

    Add Reset Button, Hash and progress bar

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,5 +1,24 @@
 // Flickity Carousel
 var flkty = new Flickity('.main-carousel', {
-    cellAlign: 'left',
-    contain: true
+    wrapAround: true,
+    fullscreen: true,
+    pageDots: false,
+    hash: true
+});
+// Button Reset
+var buttonGroup = document.querySelector('.button-reset');
+
+buttonGroup.addEventListener('click', function (event) {
+    if (!matchesSelector(event.target, '.button')) {
+        return;
+    }
+    var selector = event.target.getAttribute('data-selector');
+    flkty.selectCell(selector);
+});
+// Progress Bar
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
 });
