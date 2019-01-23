f2db914584dbf6e0e8e059b21bd388413afbfc27
Norbert Janczak <norbert.janczak03@gmail.com>
2019-01-23 22:46:43 +0100

    Add Google Maps

--- js/script.js
+++ js/script.js
@@ -1,22 +1,79 @@
+// Variables
+var sliderData;
+
+
 // Mustache 
-(function(){ 
-var templateSlider = document.getElementById('template-slider').innerHTML;
-var templateCarousel = document.getElementById('template-carousel').innerHTML;
+(function () {
+    sliderData = [{
+            sliderID: 'carousel-cell1',
+            image: 'images/carousel-image1.jpg',
+            title: 'Slider 1',
+            description: 'Lorem ipsum.',
+            coords: {
+                lat: -25.363,
+                lng: 131.044
+            }
+        },
+        {
+            sliderID: 'carousel-cell2',
+            image: 'images/carousel-image2.jpg',
+            title: 'Slider 2',
+            description: 'Lorem ipsum.',
+            coords: {
+                lat: -23.363,
+                lng: 121.044
+            }
+        },
+        {
+            sliderID: 'carousel-cell3',
+            image: 'images/carousel-image3.jpg',
+            title: 'Slider 3',
+            description: 'Lorem ipsum.',
+            coords: {
+                lat: -24.363,
+                lng: 141.044
+            }
+        },
+        {
+            sliderID: 'carousel-cell4',
+            image: 'images/carousel-image4.jpg',
+            title: 'Slider 4',
+            description: 'Lorem ipsum.',
+            coords: {
+                lat: -21.363,
+                lng: 148.044
+            }
+        },
+        {
+            sliderID: 'carousel-cell5',
+            image: 'images/carousel-image5.jpg',
+            title: 'Slider 5',
+            description: 'Lorem ipsum.',
+            coords: {
+                lat: -26.363,
+                lng: 118.044
+            }
+        }
+    ]
 
-Mustache.parse(templateCarousel);
+    var templateSlider = document.getElementById('template-slider').innerHTML;
+    var templateCarousel = document.getElementById('template-carousel').innerHTML;
 
-var generatedSlider = '';
+    Mustache.parse(templateCarousel);
 
-for(var i = 0; i < sliderData.length; i++){
-   generatedSlider += Mustache.render(templateCarousel, sliderData[i])
-}
+    var generatedSlider = '';
 
-var fullSliderItem = Mustache.render(templateSlider, {slider: generatedSlider});
+    for (var i = 0; i < sliderData.length; i++) {
+        generatedSlider += Mustache.render(templateCarousel, sliderData[i])
+    }
 
-var results = document.getElementById('results');
-results.insertAdjacentHTML('beforeend', fullSliderItem);
+    var fullSliderItem = Mustache.render(templateSlider, {
+        slider: generatedSlider
+    });
+
+    var results = document.getElementById('results');
+    results.insertAdjacentHTML('beforeend', fullSliderItem);
 })();
-
 
 // Flickity Carousel
 var flkty = new Flickity('.main-carousel', {
@@ -25,6 +82,7 @@ var flkty = new Flickity('.main-carousel', {
     pageDots: false,
     hash: true
 });
+
 // Button Reset
 var buttonGroup = document.querySelector('.button-reset');
 
@@ -35,6 +93,7 @@ buttonGroup.addEventListener('click', function (event) {
     var selector = event.target.getAttribute('data-selector');
     flkty.selectCell(selector);
 });
+
 // Progress Bar
 var progressBar = document.querySelector('.progress-bar')
 
@@ -42,3 +101,20 @@ flkty.on('scroll', function (progress) {
     progress = Math.max(0, Math.min(1, progress));
     progressBar.style.width = progress * 100 + '%';
 });
+
+// Google Maps
+(function () {
+    window.initMap = function () {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: sliderData[0].coords,
+        });
+
+        for (var i = 0; i < sliderData.length; i++) {
+            var marker = new google.maps.Marker({
+                position: sliderData[i].coords,
+                map: map
+            });
+        };
+    }
+})();
